a30d7ccf3dcd1f2ec09bd06064e8b387b4ea8827
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-03-16 16:27:08 -0500

    complete organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 3, 'M', 'Brown', 'gentle', true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 7, 'M', 'Brown', 'gentle', true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 5, 'F', 'Brown', 'gentle', true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 9, 'M', 'Brown', 'gentle', true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 6, 'F', 'Brown', 'gentle', true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 5, 'M', 'Brown', 'gentle', true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 8, 'F', 'Brown', 'gentle', true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 2, 'M', 'Brown', 'gentle', true);
